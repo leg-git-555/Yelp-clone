439f3d27d30032ce7088a0e6b6c8391b856b8906
Nicholas Leger <nicholasleger4@gmail.com>
2024-03-01 12:24:22 -0500

    showing all businesses on frontend

--- react-vite/src/redux/store.js
+++ react-vite/src/redux/store.js
@@ -6,9 +6,11 @@ import {
 } from "redux";
 import thunk from "redux-thunk";
 import sessionReducer from "./session";
+import { businessesReducer } from "./businesses";
 
 const rootReducer = combineReducers({
   session: sessionReducer,
+  businesses: businessesReducer
 });
 
 let enhancer;
--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from 'react-router-dom';
 import LoginFormPage from '../components/LoginFormPage';
 import SignupFormPage from '../components/SignupFormPage';
 import Layout from './Layout';
+import { Homepage } from '../components/Homepage/Homepage';
+import { Test } from '../components/Test/Test';
 
 export const router = createBrowserRouter([
   {
@@ -9,7 +11,7 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <h1>Welcome!</h1>,
+        element: <Homepage />,
       },
       {
         path: "login",
@@ -18,6 +20,10 @@ export const router = createBrowserRouter([
       {
         path: "signup",
         element: <SignupFormPage />,
+      },
+      {
+        path: "test",
+        element: <Test />,
       },
     ],
   },
