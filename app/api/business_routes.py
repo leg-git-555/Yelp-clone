2554532c500301be613714be1824f683f074c86b
Nicholas Leger <nicholasleger4@gmail.com>
2024-03-01 16:57:26 -0500

    add create business backend

--- app/api/business_routes.py
+++ app/api/business_routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request, redirect
 from flask_login import login_required, current_user
-from ..models import Business
+from ..models import Business, db
 from ..forms import BusinessForm
+from .aws_helpers import upload_file_to_s3, get_unique_filename
 
 business_routes = Blueprint("businesses", __name__)
 
@@ -13,3 +14,46 @@ def businesses():
     businesses = Business.query.all()
     #add comment
     return {'businesses': [business.to_dict() for business in businesses]}
+
+@business_routes.route("/new", methods=['POST'])
+@login_required
+def create_business():
+    """Create a new business"""
+    form = BusinessForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    #user_id is int type
+    user_id = current_user.to_dict()['id']
+
+    if form.validate_on_submit(): 
+        image = form.data['image_url']
+        url = None
+        
+        if image:
+                image.filename = get_unique_filename(image.filename)
+                upload = upload_file_to_s3(image)
+
+                if "url" not in upload:
+                    return {
+                            "message": "error occured during aws upload. idk",
+                            "upload": upload
+                            }
+                
+                url = upload['url']
+
+        business = Business(
+             name=form.data["name"],
+             address=form.data["address"],
+             city=form.data["city"],
+             state=form.data["state"],
+             country=form.data["country"],
+             category=form.data["category"],
+             price=form.data["price"],
+             image_url=url,
+             owner_id=user_id
+        )
+        db.session.add(business)
+        db.session.commit()
+        return business.to_dict()
+
+    return {"message": "business wasn't created"}
